a6b1be1b55232707eaaff5aa173b9fa4e43e439f
Adelchino Simone <a.simone@wwg.it>
2017-10-24 22:46:34 +0200

    Added CategoryRESTRepository

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import CategoryRESTRepository from '../repositories/REST/CategoryRESTRepository';
+
 class Home extends React.Component {
   render() {
+    var categoryRESTRepository = new CategoryRESTRepository();
+    categoryRESTRepository.get();
+
     return (
       <ul className="categories-names-list">
         <li className="category-name">Category1</li>
---
+++ src/repositories/REST/CategoryRESTRepository.js
@@ -0,0 +1,19 @@
+class CategoryRESTRepository {
+  constructor() {
+    let token = Math.random().toString(36).substr(-8);
+
+    this.address = "http://localhost:3001";
+    this.headers = {
+      'Accept': 'application/json',
+      'Authorization': token
+    }
+  }
+
+  get() {
+    fetch(`${this.address}/categories`, { headers: this.headers })
+      .then(res => res.json())
+      .then(data => data.categories);
+  }
+}
+
+export default CategoryRESTRepository
