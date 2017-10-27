8bd944ea0b0f7977d5cda752b8fb47f716894ef3
Adelchino Simone <a.simone@wwg.it>
2017-10-27 23:24:53 +0200

    Added dynamic categoryName to ProductCategory component

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -5,14 +5,11 @@ import { connect } from 'react-redux';
 class Home extends React.Component {
   render() {
     return (
-      <div>
       <ul className="categories-names-list">
         {this.props.categories && this.props.categories.map((category, idx) => (
           <li className="category-name" key={idx}><Link to={"/category/" + category.path}>{category.name}</Link></li>
         ))} 
       </ul>
-      <Link to="/category/react">REACT</Link>
-      </div>
     )
   }
 }
--- src/containers/ProductCategory.js
+++ src/containers/ProductCategory.js
@@ -1,17 +1,31 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class ProductCategory extends React.Component {
   render() {
     const categoryPath = this.props.match.params.categoryPath;
+    let categoryName = "";
+    
+    if(this.props.categories) {
+      categoryName = this.props.categories.find(category => category.path === categoryPath).name;
+    }
 
     return (
       <div>
-        <h1>{categoryPath}</h1>
+        <h1>{categoryName}</h1>
         <Link to="/">Go to home</Link>
       </div>
     );
   }
 }
 
-export default ProductCategory
+function mapStateToProps({ categories }) {
+  return {
+    categories
+  }
+}
+
+export default connect(
+  mapStateToProps
+) (ProductCategory)
