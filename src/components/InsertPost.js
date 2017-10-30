1d25c4ceeeb928d899040ae6f467d3aafab342ec
Adelchino Simone <a.simone@wwg.it>
2017-10-30 22:00:30 +0100

    InsertPost > Insert a complete post object

--- src/components/InsertPost.js
+++ src/components/InsertPost.js
@@ -13,8 +13,33 @@ class InsertPost extends React.Component {
     this.setState({ title });
   }
 
+  updateAuthor = (author) => {
+    this.setState({ author });
+  }
+
+  updateCategory = (category) => {
+    this.setState({ category });
+  }
+
+  updateBody = (body) => {
+    this.setState({ body });
+  }
+
   addPost = () => {
     var post = new Post(this.state.title);
+
+    if (this.state.author) {
+      post.author = this.state.author;
+    }
+
+    if (this.state.category) {
+      post.category = this.state.category;
+    }
+
+    if (this.state.body) {
+      post.body = this.state.body;
+    }
+
     this.props.addPost(post);
   }
 
@@ -27,11 +52,31 @@ class InsertPost extends React.Component {
       <div>
         <b>Insert a new post:</b>
         <form onSubmit={this.handleSubmit}>
-          <input type="text" onChange={(event) => this.updateTitle(event.target.value)} value={this.state.title} />
+          Title<br />
+          <input type="text" onChange={(event) => this.updateTitle(event.target.value)} value={this.state.title} /><br />
+          Author<br />
+          <input type="text" onChange={(event) => this.updateAuthor(event.target.value)} value={this.state.author} /><br />
+          Category <br />
+          <select value={this.state.category} onChange={(event) => this.updateCategory(event.target.value)}>
+            <option></option>
+            {this.props.categories && Object.values(this.props.categories).map(
+              category => (<option value={category.name}>{category.name}</option>)
+            )}
+          </select>
+          <br />
+          Body<br />
+          <textarea value={this.state.body} onChange={(event) => this.updateBody(event.target.value)} />
+          <br />
           <button onClick={this.addPost}>Add</button>
         </form>
       </div>
     );
+  }
+}
+
+function mapStateToProps({ categories }) {
+  return {
+    categories
   }
 }
 
@@ -42,6 +87,6 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(InsertPost);
