40d05ea5d8754f423a6c7606406700c998468f64
Adelchino Simone <a.simone@wwg.it>
2017-11-07 21:24:55 +0100

    Adde posts sort selector on PostsList

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -5,8 +5,11 @@ import PropTypes from 'prop-types';
 
 import { getAsyncPosts } from '../actions/posts';
 
+const SORT_BY_VOTE_SCORE = "voteScore";
+const SORT_BY_TIMESTAMP = "timestamp";
+
 function sortPosts(posts, field) {
-  if(!posts) {
+  if (!posts) {
     return posts;
   }
 
@@ -19,6 +22,11 @@ class PostsList extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      posts: [],
+      postsSort: SORT_BY_VOTE_SCORE
+    }
+
     this.props.getPosts();
   }
 
@@ -26,30 +34,49 @@ class PostsList extends React.Component {
     category: PropTypes.string
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.posts) {
+      this.setState({ posts: sortPosts(Object.values(nextProps.posts), SORT_BY_VOTE_SCORE) });
+    }
+  }
+
+  updateSort(field) {
+    this.setState({
+      postsSort: field,
+      posts: sortPosts(this.state.posts, field)
+    });
+  }
+
   render() {
     let posts = [];
 
-    if(this.props.posts) {
-      posts = this.props.posts;
+    if (this.state.posts) {
+      posts = this.state.posts;
 
-      if(this.props.category) {
+      if (this.props.category) {
         posts = posts.filter(post => post.category === this.props.category);
       }
     }
 
     return (
-      <ul className="posts-list">
-        {posts.map((post, idx) => (
-          <li key={idx}><Link to={"/post/" + post.id}>{post.title} (score: {post.voteScore})</Link></li>
-        ))}
-      </ul>
+      <div>
+        <select value={this.state.postsSort} onChange={(event) => this.updateSort(event.target.value)}>
+          <option value={SORT_BY_VOTE_SCORE}>{SORT_BY_VOTE_SCORE}</option>
+          <option value={SORT_BY_TIMESTAMP}>{SORT_BY_TIMESTAMP}</option>
+        </select>
+        <ul className="posts-list">
+          {posts.map((post, idx) => (
+            <li key={idx}><Link to={"/post/" + post.id}>{post.title} (score: {post.voteScore})</Link></li>
+          ))}
+        </ul>
+      </div>
     );
   }
 }
 
 function mapStateToProps({ posts }) {
   return {
-    posts: sortPosts(Object.values(posts), "voteScore")
+    posts
   }
 }
 
